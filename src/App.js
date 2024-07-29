fdbb2ecc49c33ae5500914c669a5797b5efeb47e
vladibo13 <vladibo13@gmail.com>
2024-07-29 10:02:20 +0300

    finished ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonWrapper from "./components/ButtonWrapper";
+import Button from "./components/Button";
+import React, { useState } from "react";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
         <Screen value="0" />
-        <ButtonWrapper />
+        <ButtonWrapper>
+          {btnValues.flat().map((btnValue, i) => {
+            return (
+              <Button
+                key={i}
+                className={btnValue === "=" ? "equals" : ""}
+                value={btnValue}
+                onClick={() => {
+                  console.log(`Button ${i} clicked!`);
+                }}
+              ></Button>
+            );
+          })}
+        </ButtonWrapper>
       </Wrapper>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Button.css";
+
+const Button = ({ className, value, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Button;
